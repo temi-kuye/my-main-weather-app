8685d07392b2850b11a2ed10503e6e3afcb7833e
temi-kuye <temi.kuye@gmail.com>
2022-12-09 08:47:23 +0100

    Worked on getting the forecast from the longitude and latitude

--- styles/weathers.js
+++ styles/weathers.js
@@ -42,6 +42,13 @@ forecastHTML = forecastHTML + `</div>`;
 forecast.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+    let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
+    let apiUrl =  `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
+
   function displayTemp(response) {
     let temperature = document.querySelector("#temp");
     let city = document.querySelector("#city");
@@ -63,12 +70,13 @@ forecast.innerHTML = forecastHTML;
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
     weatherIcon.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
   }
   
   function search(city) {
     let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  
     axios.get(apiUrl).then(displayTemp);
   }
   
@@ -105,6 +113,5 @@ forecast.innerHTML = forecastHTML;
   
   let celsius = document.querySelector("#celsius-link");
   celsius.addEventListener("click", convertCelsiusTemp);
-  
+
   search("Lagos"); 
-  displayForecast();
