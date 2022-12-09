5853b9eb36a4d3d4c67103bfd482d79336473873
temi-kuye <temi.kuye@gmail.com>
2022-12-09 02:14:22 +0100

    Showing current day and time

--- styles/weathers.js
+++ styles/weathers.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let weekDays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thurdsay",
+      "Friday",
+      "Saturday"
+    ];
+    let day = weekDays[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+  }
+
 function displayTemp(response) {
     let temperature = document.querySelector("#temp");
     let city = document.querySelector("#city");
@@ -9,6 +32,7 @@ function displayTemp(response) {
     condition.innerHTML = response.data.weather[0].description;
     humidity.innerHTML = Math.round(response.data.main.humidity);
     wind.innerHTML = Math.round(response.data.wind.speed);
+    date.innerHTML = formatDate(response.data.dt * 1000);
   }
   
   let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
