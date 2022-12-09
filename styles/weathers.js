bf1fe25b0d15d67f0f083e80bdf61677209a2a72
temi-kuye <temi.kuye@gmail.com>
2022-12-09 02:46:53 +0100

    Added the icon from the api

--- styles/weathers.js
+++ styles/weathers.js
@@ -20,23 +20,31 @@ function formatDate(timestamp) {
     let day = weekDays[date.getDay()];
     return `${day} ${hours}:${minutes}`;
   }
-
-function displayTemp(response) {
+  
+  function displayTemp(response) {
     let temperature = document.querySelector("#temp");
     let city = document.querySelector("#city");
     let condition = document.querySelector("#condition");
     let humidity = document.querySelector("#humidity");
     let wind = document.querySelector("#windSpeed");
+    let todaysDate = document.querySelector("#date");
+    let weatherIcon = document.querySelector("#icon");
     temperature.innerHTML = Math.round(response.data.main.temp);
     city.innerHTML = response.data.name;
     condition.innerHTML = response.data.weather[0].description;
     humidity.innerHTML = Math.round(response.data.main.humidity);
     wind.innerHTML = Math.round(response.data.wind.speed);
-    date.innerHTML = formatDate(response.data.dt * 1000);
+    todaysDate.innerHTML = formatDate(response.data.dt * 1000);
+    weatherIcon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    weatherIcon.setAttribute("alt", response.data.weather[0].description);
   }
   
   let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+  let city = "sydney";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   
   axios.get(apiUrl).then(displayTemp);
   
